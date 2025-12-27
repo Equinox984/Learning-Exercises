e9cf1b6c91d38eeb32df2fcb9f04dd214053f30e
Equinox984 <lopezkris984@proton.me>
2025-12-26 19:44:46 -0600

    Improve get_number function with input validation

--- CS50P/cat.py
+++ CS50P/cat.py
@@ -16,10 +16,12 @@ def main():
 
 def get_number():
     while True:
-        n = int(input("What's n? "))
-        if n > 0:
-            break
-    return n
+        try:
+            n = int(input("What's n? "))
+            if n > 0:
+                return n
+        except ValueError:
+            print("Write a number dude!")
 
 
 def meow(n):
