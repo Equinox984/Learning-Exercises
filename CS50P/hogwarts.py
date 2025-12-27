fa6fa582da5dc22a1efbbb1c3efc4fa6c2263d9b
Equinox984 <lopezkris984@proton.me>
2025-12-27 15:11:50 -0600

    Add CS50P Work

---
+++ CS50P/hogwarts.py
@@ -0,0 +1,27 @@
+"""students = ["Hermione", "Harry", "Ron", "Draco"]"""
+
+"""for i in range(len(students)):
+    print(i + 1, students[i])"""
+
+############################################################################
+"""students = {
+    "Hermione": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor",
+    "Draco": "Slytherin",
+}
+
+for student in students:
+    print(student, students[student], sep=", ")"""
+
+###########################################################################
+
+students = [
+    {"Name": "Hermione", "House": "Gryffindor", "Patronus": "Otter"},
+    {"Name": "Harry", "House": "Gryffindor", "Patronus": "Stag"},
+    {"Name": "Ron", "House": "Gryffindor", "Patronus": "Jack Russell Terrier"},
+    {"Name": "Draco", "House": "Slytherin", "Patronus": None},
+]
+
+for student in students:
+    print(student["Name"], student["House"], student["Patronus"], sep=", ")
