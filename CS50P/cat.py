f296f3a742e8e83ad7478551513d28cac06458ff
Equinox984 <lopezkris984@proton.me>
2025-12-26 19:30:53 -0600

    Add CS50P work

--- CS50P/cat.py
+++ CS50P/cat.py
@@ -3,5 +3,28 @@ while i < 3:
     i += 1
     print("Meow")"""
 
-for _ in range(3):
-    print("Meow")
+"""for _ in range(3):
+    print("Meow")"""
+
+"""print("Meow\n" * 3, end="")"""
+
+
+def main():
+    number = get_number()
+    meow(number)
+
+
+def get_number():
+    while True:
+        n = int(input("What's n? "))
+        if n > 0:
+            break
+    return n
+
+
+def meow(n):
+    for _ in range(n):
+        print("Meow")
+
+
+main()
