89bf9ff87396c7c80e85192b605028a1b2eb0bb1
Equinox984 <lopezkris984@proton.me>
2026-01-01 19:32:01 -0600

    Add CS50P Work

--- CS50P/number.py
+++ CS50P/number.py
@@ -1,5 +1,15 @@
-try:
-    x = float(input("What's x? "))
-    print(f"x is {x}")
-except ValueError:
-    print("x isn't a number.")
+def main():
+    x = get_int("What's x? ")
+    print(f"x is {x}\n")
+
+
+def get_int(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            pass  # We catch the error but we don't do anything about it.
+            # We just stay in the loop
+
+
+main()
