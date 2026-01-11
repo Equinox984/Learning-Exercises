eb54f4610950c4d889c06c07c320dc536f4aa609
Equinox984 <lopezkris984@proton.me>
2026-01-11 15:13:52 -0600

    Added C Section

---
+++ C/pun.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main (void)
+// {
+//     printf("To C, or not to C: that is the question.\n");
+//     return 0;
+// }
+//
+{
+    printf("To C, or not to C: ");
+    printf("that is the question.\n");
+    printf("\nBrevity is the soul of wit.\n --Shakespeare\n");
+}
